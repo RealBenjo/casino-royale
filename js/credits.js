c0acede333f140088da5845886ba93de9f0277d5
Benjamin Ipavec <benjamin.ipavec@gmail.com>
2026-05-18 19:43:37 +0200

    tiny tiny fix

--- js/credits.js
+++ js/credits.js
@@ -3,7 +3,7 @@ function showCredits() {
     title: 'Credits',
     html: 'This site is made by <br><strong>Benjamin Ipavec</strong>',
     icon: 'info',
-    iconColor: '#d4af37', // Sets the default info 'i' icon to your gold accent
+    iconColor: '#d4af37', // Sets the default info 'i' icon to gold
     customClass: {
       popup: 'site-swal-popup',
       title: 'site-swal-title',
@@ -11,6 +11,6 @@ function showCredits() {
       confirmButton: 'site-swal-confirm',
       actions: 'site-swal-actions'
     },
-    buttonsStyling: false // Crucial: Tells SweetAlert2 to drop its default button styles so yours can take over
+    buttonsStyling: false
   });
 }
